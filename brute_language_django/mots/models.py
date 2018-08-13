033374cee1724e7c9ab175e389cd32b4c43a524c
Guy Lifshitz <guylifshitz@gmail.com>
2018-08-14 00:08:55 +0200

    + Create basic models + Import GLAWI code

--- brute_language_django/mots/models.py
+++ brute_language_django/mots/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    word = models.CharField(max_length=100)
+    wiki_id = models.IntegerField()
+
+class Lemme(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    word = models.CharField(max_length=100)
+    pronunciation = models.CharField(max_length=100)
+
+    word_type = models.CharField(
+        max_length=10,
+    )
+
+
+    # MASCULIN = 'm'
+    # FEMENIN = 'f'
+    # EPICENE = 'e'
+    # GENDER_CHOICES = (
+    #     (MASCULIN, 'm'),
+    #     (FEMENIN, 'f'),
+    #     (EPICENE, 'e'),
+    # )
+    gender = models.CharField(
+        max_length=1,
+        # choices=GENDER_CHOICES,
+    )
+
+    # SINGULAR = 's'
+    # PLURAL = 'p'
+    # INVARIABLE = 'i'
+    # NUMBER_CHOICES = (
+    #     (SINGULAR, 's'),
+    #     (PLURAL, 'p'),
+    #     (INVARIABLE, 'i'),
+    # )
+    number = models.CharField(
+        max_length=2,
+        # choices=NUMBER_CHOICES,
+    )
+
+    lemma = models.BooleanField()
+    lemmaType = models.CharField(max_length=10)
+
+class Definition(models.Model):
+    word = models.ForeignKey(Lemme, on_delete=models.CASCADE)
+    definition = models.TextField()
+    
